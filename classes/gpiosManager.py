3cf63e835a0f642578730841a662154adeb1e4e0
David Diaz <david.diaz190799@gmail.com>
2025-06-27 20:43:25 -0500

    update gpio rasppberyy

--- classes/gpiosManager.py
+++ classes/gpiosManager.py
@@ -1,51 +1,37 @@
-import RPi.GPIO as GPIO
+import os
+from gpiozero import Device
+from gpiozero import DigitalOutputDevice
+from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
+Device.pin_factory = PiGPIOFactory()
 class GpiosManager():
     def __init__(self):
         super().__init__()
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        self.relay_1 = 20
-        self.relay_2 = 21
-        self.relay_3 = 16
-        self.relay_4 = 12
+        self.relay_1 = DigitalOutputDevice(20)
+        self.relay_2 = DigitalOutputDevice(21)
+        self.relay_3 = DigitalOutputDevice(16)
+        self.relay_4 = DigitalOutputDevice(12)
         # declaracion de salidas
-        GPIO.setup(self.relay_1, GPIO.OUT)
-        GPIO.setup(self.relay_2, GPIO.OUT)
-        GPIO.setup(self.relay_3, GPIO.OUT)
-        GPIO.setup(self.relay_4, GPIO.OUT)
-        #inicializacion
-        GPIO.output(self.relay_1,GPIO.HIGH)
-        GPIO.output(self.relay_2,GPIO.HIGH)
-        GPIO.output(self.relay_3,GPIO.HIGH)
-        GPIO.output(self.relay_4,GPIO.HIGH)
+        self.relay_1.on()
+        self.relay_2.on()
+        self.relay_3.on()
+        self.relay_4.on()
+
 
     def turnstileOpen(self):
-        GPIO.output(self.relay_1, GPIO.LOW)
-        GPIO.output(self.relay_2, GPIO.LOW)
-        GPIO.output(self.relay_3, GPIO.LOW)
-        GPIO.output(self.relay_4, GPIO.LOW)
+        self.relay_1.off()
+        self.relay_2.off()
         time.sleep(0.5)
-        GPIO.output(self.relay_1, GPIO.HIGH)
-        GPIO.output(self.relay_2, GPIO.HIGH)
-        GPIO.output(self.relay_3, GPIO.HIGH)
-        GPIO.output(self.relay_4, GPIO.HIGH)
+        self.relay_1.on()
+        self.relay_2.on()
         return "puerta general abierta"
 
     def armDown(self):
-        GPIO.output(self.relay_3, GPIO.LOW)
-        GPIO.output(self.relay_4, GPIO.LOW)
+        self.relay_3.off()
+        self.relay_4.off()
         time.sleep(0.5)
-        GPIO.output(self.relay_3, GPIO.HIGH)
-        GPIO.output(self.relay_4, GPIO.HIGH)
+        self.relay_3.on()
+        self.relay_4.on()
         return "puerta especial abierta"
 
-# class GpiosManager():
-#     def __init__(self):
-#         super().__init__()
-        
-
-#     def turnstileOpen(self):
-#         print("Abriendo puerta general")
-#         return "puerta general abierta"
